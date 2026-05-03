4d16eb886b93023af13819286951766cfd3fe64b
Никита Кобылко <nickoob@mail.ru>
2026-05-03 18:53:10 +0300

    Changed Stats.h

--- Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/Stats.h
+++ Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/Stats.h
@@ -3,36 +3,18 @@
 #pragma once
 
 struct Stats {
-  const int WARRIOR_START_HP = 100;
-  const int WARRIOR_START_ATK = 20;
-  const int WARRIOR_START_SPD = 15;
-  const int WARRIOR_START_INT = 10;
-  const int WARRIOR_START_MP = 50;
+  int MaxHP = 100;
+  int CurrentHP = 100;
+  int MaxMP = 50;
+  int CurrentMP = 50;
 
-  const int ROGUE_START_HP = 80;
-  const int ROGUE_START_ATK = 15;
-  const int ROGUE_START_SPD = 20;
-  const int ROGUE_START_INT = 15;
-  const int ROGUE_START_MP = 75;
+  int BonusATK = 0;
+  int BonusSPD = 0;
+  int BonusINT = 0;
+  int BonusMaxHP = 0;
+  int BonusMaxMP = 0;
 
-  const int MAGE_START_HP = 70;
-  const int MAGE_START_ATK = 10;
-  const int MAGE_START_SPD = 13;
-  const int MAGE_START_INT = 20;
-  const int MAGE_START_MP = 100;
-
-  int MaxHealth = 100;
-  int CurrentHealth = 100;
-  int MaxMana = 50;
-  int CurrentMana = 50;
-
-  int BonusAttack = 0;
-  int BonusSpeed = 0;
-  int BonusIntellect = 0;
-  int BonusMaxHealth = 0;
-  int BonusMaxMana = 0;
-
-  int GetTotalAttack() const { return /*Аттака*/ +BonusAttack; }
-  int GetTotalSpeed() const { return /*Скорость*/ +BonusSpeed; }
-  int GetTotalIntellect() const { return /*Интеллект*/ +BonusIntellect; }
+  //int GetTotalAttack() const { return /*Аттака*/ +BonusAttack; }
+  //int GetTotalSpeed() const { return /*Скорость*/ +BonusSpeed; }
+  //int GetTotalIntellect() const { return /*Интеллект*/ +BonusIntellect; }
 };
