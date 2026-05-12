e9613fb253c70a414bed575562fe7aa6bb5cb79c
kiriread <bulatmub1@gmail.com>
2026-05-12 17:58:55 +0300

    Changed: - BattleManager - Enemy - EnemyCreator

--- Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/BossRoom.h
+++ Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/BossRoom.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include "Room.h"
+
+class BossRoom : public Room {
+public:
+	BossRoom();
+	void OnEnter(Player* player) override;
+};
--- Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/Enemy.h
+++ Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/Enemy.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <string>
+#include <vector>
+
+struct Enemy {
+	std::string Name;
+	int HP;
+	int ATK;
+	int SPD;
+};
+std::vector<Enemy> RegularEnemy() {
+	return {
+		{"„C„€„q„|„y„~ „ƒ „{„y„~„w„p„|„€„}", 30, 20, 5},
+		{"„O„‚„{ „ƒ „t„…„q„y„~„€„z", 50, 15, 3},
+		{"„R„|„y„x„u„~„", 60, 16, 2}
+	};
+}
+std::vector<Enemy> EliteEnemy() {
+	return {
+		{"„S„‚„€„|„ „Š„p„}„p„~", 50, 40, 5},
+		{"„O„s„‚ „}„p„s", 70, 30, 3}
+	};
+}
+std::vector<Enemy> Bosses() {
+	return {
+		{"„X„v„‚„~„„z „t„‚„p„{„€„~", 90, 50, 5},
+		{"„D„u„}„€„~ „{„‚„€„r„y", 70, 30, 8}
+	};
+}
