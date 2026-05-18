0be36e0f8aae134ba8f306cc004ac17e0b678a86
kiriread <bulatmub1@gmail.com>
2026-05-18 17:05:06 +0300

    Added Spell system during the battle

--- Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/BattleManager.h
+++ Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/BattleManager.h
@@ -5,19 +5,32 @@
 #include "DataManager.h"
 #include "ConsoleUtils.h"
 #include "Game.h"
+#include "Spell.h"
 
 class BattleManager {
  private:
   Player* m_player;
   Enemy* m_enemy;
   Game* m_game;
+  Spell m_sp;
   int m_pInitiative;
   int m_eInitiative;
   bool m_isPlayerTurn;
+  int m_totalATK;
+  int m_totalSPD;
+  int m_totalEnemyATK;
+  int m_totalEnemySPD;
+  int m_rage_dur;
+  int m_regen_dur;
+  int m_spdPotion_dur;
+  int m_dusk_dur;
+  int m_frost_dur;
+  std::vector<Spell> m_spells;
   DataManager m_dataManager;
   ConsoleUtils m_console;
 
   void PlayerTurn();
+  void SpellChoice();
   void EnemyTurn();
 
  public:
---
+++ Slay_the_Labyrinth_Project/Slay_the_Labyrinth_Project/Spell.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <string>
+#include "DataManager.h"
+#include "Player.h"
+#include "Enemy.h"
+
+class Spell {
+private:
+	std::string m_name;
+	int m_CurDur;
+	int m_MaxDur;
+	int m_cost;
+
+	DataManager m_dataManager;
+public:
+	Spell(std::string name, int cur_dur, int max_dur, int cost);
+
+	std::string GetName() const { return m_name; }
+	int GetCurDur() const { return m_CurDur; }
+	int GetMaxDur() const { return m_MaxDur; }
+	int GetCost() const { return m_cost; }
+
+	std::vector<Spell> GetSpells(std::string class_name);
+
+	int Rage(Player* player);
+	int Sweeping_strike();
+	int Regeneration_potion();
+	int Backstab();
+	int Speed_potion(Player* player);
+	int Dust_in_eyes(Enemy* enemy);
+	int Fireball();
+	int Frost_vortex(Enemy* enemy);
+	int Healing();
+
+	std::vector<Spell> WarriorSpells();
+	std::vector<Spell> RogueSpells();
+	std::vector<Spell> MageSpells();
+};
